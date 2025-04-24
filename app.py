c1ce5d581977183bb0377c8e9893fdc89a7eff72
kenovenas <136008924+kenovenas@users.noreply.github.com>
2025-04-24 02:58:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask
-from replicador_simples import iniciar_replicador
 import threading
+from replicador_simples import main  # Usamos a função principal corretamente
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Servidor Flask do replicador está rodando!'
+    return 'Servidor replicador está rodando!'
+
+def iniciar_bot():
+    main()
+
+# Iniciar o bot em thread separada
+threading.Thread(target=iniciar_bot).start()
 
 if __name__ == '__main__':
-    try:
-        threading.Thread(target=iniciar_replicador).start()
-        app.run(host='0.0.0.0', port=10000)
-    except Exception as e:
-        print('[ERRO AO INICIAR BOT]')
-        print(e)
+    app.run(host='0.0.0.0', port=10000)
