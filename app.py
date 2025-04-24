d6c2bb8f0ff4b79efb7839001fc46dbae81d8122
kenovenas <136008924+kenovenas@users.noreply.github.com>
2025-04-24 01:54:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-
 from flask import Flask
 import asyncio
+import threading
 from replicador_simples import main, client
 
 app = Flask(__name__)
+
+loop = asyncio.new_event_loop()
+
+def start_bot():
+    asyncio.set_event_loop(loop)
+    with client:
+        loop.run_until_complete(main())
+
+@app.before_first_request
+def activate_bot():
+    thread = threading.Thread(target=start_bot)
+    thread.start()
 
 @app.route('/')
 def home():
     return "Servidor do replicador do Telegram está rodando."
 
-@app.route('/start')
-def start_replicador():
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
-    return "Bot iniciado!"
-
 if __name__ == '__main__':
-    with client:
-        client.loop.run_until_complete(main())
+    app.run(host='0.0.0.0', port=10000)
