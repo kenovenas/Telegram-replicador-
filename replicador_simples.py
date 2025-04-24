2136115ec85a8b2bb5099ac2765a55bf1a5572e4
kenovenas <136008924+kenovenas@users.noreply.github.com>
2025-04-24 01:40:56 -0300

    Add files via upload

---
+++ replicador_simples.py
@@ -0,0 +1,31 @@
+
+from telethon import TelegramClient, events
+import os
+import asyncio
+
+# === CONFIGURAÇÕES ===
+api_id = 20225004
+api_hash = "8f4c78e858658cd2aa21967a087bf819"
+phone_number = "+5519971432718"
+canal_origem = "https://t.me/+XkWYvpDc2-c5ZGQ5"
+canal_destino = "https://t.me/+RDbY2XIDf-kyNDlh"
+
+client = TelegramClient('sessao_usuario', api_id, api_hash)
+
+async def main():
+    await client.start(phone=phone_number)
+    print("Bot iniciado e monitorando o canal de origem...")
+
+    @client.on(events.NewMessage(chats=canal_origem))
+    async def handler(event):
+        try:
+            await client.send_message(canal_destino, event.message)
+            print("Mensagem replicada.")
+        except Exception as e:
+            print("Erro ao replicar mensagem:", e)
+
+    await client.run_until_disconnected()
+
+if __name__ == "__main__":
+    with client:
+        client.loop.run_until_complete(main())
